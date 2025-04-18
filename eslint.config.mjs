fd62437e977b6e807731e76095fff23950de1927
Ianka Karuzia <iankakaruzia@gmail.com>
2025-04-18 12:30:16 -0300

    chore: setup basic nest project

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+// @ts-check
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import perfectionist from 'eslint-plugin-perfectionist';
+import functional from 'eslint-plugin-functional';
+import sonarjs from 'eslint-plugin-sonarjs';
+
+export default tseslint.config(
+  {
+    ignores: ['eslint.config.mjs'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+  perfectionist.configs['recommended-natural'],
+  functional.configs.externalTypeScriptRecommended,
+  sonarjs.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'commonjs',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+    },
+  },
+  {
+    ignores: ['**/node_modules/**', '**/dist/**', 'commitlint.config.js'],
+  },
+);
